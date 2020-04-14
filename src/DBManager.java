819169d7c1a3fc46e7d2dc4b0fcb213dbce09d50
cowen <craig-owen@hotmail.com>
2020-04-14 12:27:56 +0100

    Add DBManager class

---
+++ src/DBManager.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager {
+    /**
+     * Connect to Database
+     * @author Craig Owen
+     */
+
+        public static final String URL = "jdbc:mysql://localhost:3306/testdb";
+        public static final String USER = "testuser";
+        public static final String PASS = "testpass";
+        /**
+         * Get a connection to database
+         * @return Connection object
+         */
+        public static Connection getConnection()
+        {
+            try {
+//            DriverManager.registerDriver(new Driver());
+                return DriverManager.getConnection(URL, USER, PASS);
+            } catch (SQLException ex) {
+                throw new RuntimeException("Error connecting to the database", ex);
+            }
+        }
+
+    }
+
